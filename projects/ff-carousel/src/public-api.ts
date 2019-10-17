a2ed012d94b2323c5e4c4cc10d3390b2acbab568
Marat <marat@frontend-freelancer.com>
2019-10-17 16:58:47 +0300

    .fix

--- projects/ff-carousel/src/public-api.ts
+++ projects/ff-carousel/src/public-api.ts
@@ -2,6 +2,10 @@
  * Public API Surface of ff-carousel
  */
 
-export * from './lib/ff-carousel.service';
-export * from './lib/ff-carousel.component';
 export * from './lib/ff-carousel.module';
+export * from './lib/ff-carousel.component';
+export * from './lib/ff-carousel-arrow.directive';
+export * from './lib/ff-carousel-indicator.directive';
+export * from './lib/ff-carousel-item.directive';
+
+
